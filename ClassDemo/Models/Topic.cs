03f783b143b46b1d64f8e126245dd40677553372
EvanGudmestad <eagudmestad@ranken.edu>
2024-02-07 11:13:35 -0600

    Add project files.

---
+++ ClassDemo/Models/Topic.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassDemo.Models
+{
+    public class Topic
+    {
+        public string TopicId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;    
+    }
+}
